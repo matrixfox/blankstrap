6d2b52fc94c0bc00e5317424301cc6237cc76e17
matrixfox <matrixfox@gmail.com>
2014-04-05 13:51:44 -0700

    Updating Header
    meta tags from Bootstrap and adding lib JS files.

--- header.php
+++ header.php
@@ -2,11 +2,17 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <title><?php wp_title( ' | ', true, 'right' ); ?></title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/bootstrap.min.css">
 <?php wp_head(); ?>
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 <!-- Custom Stylesheet -->
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 </head>
